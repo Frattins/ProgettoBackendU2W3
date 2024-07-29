fa288eb1e4528fcc2fab7e517b5fee38d2280024
Frattins <davide.frattini1@gmail.com>
2024-07-29 16:53:19 +0200

    Aggiungere i file di progetto.

---
+++ ProgettoBackendU2W3/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ProgettoBackendU2W3.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
